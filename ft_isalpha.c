3843e2f384379fb0168bc38227616305eb1b77d7
Ricardo <ricartoonstudio@gmail.com>
2021-07-26 23:36:05 -0300

    {ricajust}[libft]: isalpha with tests

---
+++ ft_isalpha.c
@@ -0,0 +1,7 @@
+int	ft_isalpha(int letter)
+{
+	if (letter >= 'a' && letter <= 'z')
+		return (1);
+	else
+		return (0);
+}
---
+++ test_isalpha.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int	ft_isalpha(int letter);
+
+int	main(void)
+{
+	printf("%d\n", ft_isalpha('+'));
+	return (0);
+}
