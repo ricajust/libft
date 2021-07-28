66cd899b35a80958d95653504572a00bb533fcbb
Ricardo <ricartoonstudio@gmail.com>
2021-07-27 22:17:34 -0300

    {ricajust}[libft] - wip: libft

---
+++ test_strlen.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int	ft_strlen(const char *c);
+
+int	main(void)
+{
+	printf("%d\n", ft_strlen("abcd"));
+}
---
+++ test_toupper.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int	ft_toupper(int c);
+
+int	main(void)
+{
+	printf("%c\n", ft_toupper('a'));
+}
