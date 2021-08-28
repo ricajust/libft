19f0e862aac2100306f8b4ba7dacb6a8f943e5cf
Ricardo <ricartoonstudio@gmail.com>
2021-08-28 12:32:21 -0300

    {ricajust}[libft] - criacao da ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: rda-silv <rda-silv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/22 20:03:33 by rda-silv          #+#    #+#             */
-/*   Updated: 2021/08/22 20:04:20 by rda-silv         ###   ########.fr       */
+/*   Updated: 2021/08/28 12:30:21 by rda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,28 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	
+	char	*substr;
+	size_t	lenstr;
+	size_t	i;
+	size_t	end;
+
+	if (!s)
+		return (NULL);
+	lenstr = ft_strlen(s);
+	substr = malloc(sizeof(char) * len + 1);
+	if (!substr)
+		return (NULL);
+	i = 0;
+	if (start < lenstr)
+	{
+		end = len + start;
+		while ((start < end) && (s[start] != '\0'))
+		{
+			substr[i] = s[start];
+			i++;
+			start++;
+		}
+	}
+	substr[i] = '\0';
+	return (substr);
 }
---
+++ teste.c
@@ -0,0 +1,6 @@
+int	main(void)
+{
+	int res;
+
+	res = sizeof(char);
+}
