a4419984cb60461a41b8a68e84cd5e5267855c7a
Ricardo <ricartoonstudio@gmail.com>
2021-08-29 17:27:09 -0300

    {ricajust}[libft] - Ajuste da variavel no ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: rda-silv <rda-silv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 18:02:29 by rda-silv          #+#    #+#             */
-/*   Updated: 2021/08/29 16:48:04 by rda-silv         ###   ########.fr       */
+/*   Updated: 2021/08/29 17:25:35 by rda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	start;
 	size_t	end;
-	char	substr;
+	char	*substr;
 
 	start = 0;
 	if (!s1 || !set)
 		return (NULL);
-	while (ft_strchr(set, s1[start]) && s1[start] != '\0')
+	while (ft_strchr(set, s1[start]) && (s1[start] != '\0'))
 		start++;
 	end = ft_strlen(s1);
-	while (end != 0 && ft_strchr(set, s1[end]))
+	while ((end != 0) && ft_strchr(set, s1[end]))
 		end--;
 	if ((int)(end - start + 1) <= 0)
 		return (ft_calloc(1, 1));
