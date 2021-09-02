45bb5bdef39a4253b50568bd9f083c49feba9c7c
Ricardo <ricartoonstudio@gmail.com>
2021-09-01 21:01:04 -0300

    {ricajust}[libft] - ajuste da memcpy, adicionando 2 novas variaveis

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: rda-silv <rda-silv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 20:36:08 by rda-silv          #+#    #+#             */
-/*   Updated: 2021/09/01 20:28:14 by rda-silv         ###   ########.fr       */
+/*   Updated: 2021/09/01 20:59:59 by rda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,19 @@
 
 void	*ft_memcpy(void *dt, const void *sr, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*dest;
+	unsigned char	*src;
 
 	if (!dt && !sr)
 		return (0);
+	dest = (unsigned char *)dt;
+	src = (unsigned char *)sr;
 	i = 0;
 	while (i < n)
 	{
-		((unsigned char *)dt)[i] = ((unsigned char *)sr)[i];
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
 		i++;
 	}
-	return (dt);
+	return (dest);
 }
