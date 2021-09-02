a4f40fb42072816aaf1abd1be996dbf6849d35c1
Ricardo <ricartoonstudio@gmail.com>
2021-09-01 21:21:35 -0300

    {ricajust}[libft] - Ajuste do if do strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: rda-silv <rda-silv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/22 11:23:09 by rda-silv          #+#    #+#             */
-/*   Updated: 2021/08/22 11:23:12 by rda-silv         ###   ########.fr       */
+/*   Updated: 2021/09/01 21:20:54 by rda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	i;
 	size_t	sizedest;
 
-	if (!dst && !src)
+	if (!dst || !src)
 		return (0);
 	sizedest = ft_strlen(src);
 	i = 0;
