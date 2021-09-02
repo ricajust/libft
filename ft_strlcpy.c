8be3401292453dd7ab64f1031d75b7ea364a92fa
Ricardo <ricartoonstudio@gmail.com>
2021-09-02 20:11:44 -0300

    {ricajus}[for] - Ajuste da strlcpy, trocando vetor por ponteiros

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: rda-silv <rda-silv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/22 11:23:09 by rda-silv          #+#    #+#             */
-/*   Updated: 2021/09/02 18:34:27 by rda-silv         ###   ########.fr       */
+/*   Updated: 2021/09/02 20:10:05 by rda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,11 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	if (!dst || !src)
 		return (0);
 	sizedest = ft_strlen(src);
-	i = 0;
 	if (dstsize > 0)
 	{
-		while (src[i] && i < (dstsize - 1))
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
+		while (*src && i < (dstsize - 1))
+			*(dst++) = *(src++);
+		*dst = '\0';
 	}
 	return (sizedest);
 }
