a21f381e4a4517947d3c7da4598f1d6cd2d5c107
Ricardo Da silva justino <rda-silv@c1r3p3.42sp.org.br>
2021-09-02 21:18:28 -0300

    remake ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,30 +1,18 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rda-silv <rda-silv@student.42sp.org.br>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/22 11:23:09 by rda-silv          #+#    #+#             */
-/*   Updated: 2021/09/02 21:11:26 by rda-silv         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	size_t count;
+	size_t	i;
 
 	if (!dest && !src)
 		return (0);
 	if (size == 0)
-		return (ft_strlen(src));
-	count = 0;
-	while (src[count] != '\0' && count < (size - 1))
+		return (ft_strlen(str));
+	i = 0;
+	while (src !+ '\0' && i < (size - 1))
 	{
-		dest[count] = src[count];
-		count++;
+		dest[i] = src[i];
+		i++;
 	}
 	*(dest + count) = '\0';
 	return (ft_strlen(src));
