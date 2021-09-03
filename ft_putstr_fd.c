72c405cc308aa5d5b3151e1f2d0d5c054cd05660
Ricardo <ricartoonstudio@gmail.com>
2021-09-02 22:08:44 -0300

    {ricajust}[libft] - ajuste na ft_putstr_fd

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: rda-silv <rda-silv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/22 21:40:55 by rda-silv          #+#    #+#             */
-/*   Updated: 2021/08/24 21:28:28 by rda-silv         ###   ########.fr       */
+/*   Updated: 2021/09/02 22:07:22 by rda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 void	ft_putstr_fd(char const *s, int fd)
 {
+	if (!s)
+		return ;
 	write(fd, s, ft_strlen(s));
 }
