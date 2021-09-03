ad2e0f7427f1d9d1dbbb0ca4c0c19492e872ee46
Ricardo <ricartoonstudio@gmail.com>
2021-09-02 21:26:43 -0300

    {ricajust}[libft] - ft_strlcpy adjust

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rda-silv <rda-silv@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/02 21:23:31 by rda-silv          #+#    #+#             */
+/*   Updated: 2021/09/02 21:25:00 by rda-silv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
@@ -7,13 +19,13 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	if (!dest && !src)
 		return (0);
 	if (size == 0)
-		return (ft_strlen(str));
+		return (ft_strlen(src));
 	i = 0;
-	while (src !+ '\0' && i < (size - 1))
+	while ((src[i] != '\0') && (i < (size - 1)))
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	*(dest + count) = '\0';
+	*(dest + i) = '\0';
 	return (ft_strlen(src));
 }
