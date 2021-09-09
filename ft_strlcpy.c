b022258f8481495504c3f9a1fd082c94d8619803
Ricardo <ricartoonstudio@gmail.com>
2021-09-09 14:49:00 -0300

    {ricajust}[libft] - ajuste na strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: rda-silv <rda-silv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/02 21:23:31 by rda-silv          #+#    #+#             */
-/*   Updated: 2021/09/06 15:13:13 by rda-silv         ###   ########.fr       */
+/*   Updated: 2021/09/09 14:45:21 by rda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,15 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 
 	if (!dest || !src)
 		return (0);
-	if (size == 0)
-		return (ft_strlen(src));
 	i = 0;
-	while ((src[i] != '\0') && (i < (size - 1)))
+	if (size)
 	{
-		dest[i] = src[i];
-		i++;
+		while ((src[i] != '\0') && (i < (size - 1)))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		*(dest + i) = '\0';
 	}
-	*(dest + i) = '\0';
 	return (ft_strlen(src));
 }
